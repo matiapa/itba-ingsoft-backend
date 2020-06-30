104bf7a8561b2a6dd7beb0fb12844e89cc1b75a3
MatiMonaco <matimonaco@outlook.com>
2020-06-30 17:19:29 -0300

    update bids query

--- db/queries/bid.js
+++ db/queries/bid.js
@@ -4,13 +4,19 @@ const knex = require("../knex.js");
 
 module.exports = {
   createBid(info) {
-    return knex("lot").insert(info, "*");
+    return knex("bit").insert(info, "*");
   },
 
   deleteBid(auc_id) {
     return knex("bid").where("auc_id", auc_id).del();
   },
+
   getBidById(user_id) {
-    return knex("bid").where("user_id", user_id);
+    return knex()
+      .select("id", "user_id", "auc_id", "amount", "time")
+      .from("bid")
+      .innerJoin("personal_info", "personal_info.user_id", "bid.user_id")
+      .where("bid.user_id", user_id)
+      .first();
   },
 };
