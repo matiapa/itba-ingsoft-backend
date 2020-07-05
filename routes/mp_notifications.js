d4d1f6f7e5dcf221ed073acd45d72383e5b0a747
MatiMonaco <matimonaco@outlook.com>
2020-07-05 19:35:50 -0300

    update

--- routes/mp_notifications.js
+++ routes/mp_notifications.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const router = express.Router();
 
+const router = express.Router();
+const Lot = require("../db/queries/lot.js");
 router.post("/notifications", (req, res) => {
   res.status(201).json(req.query).end();
+  Lot.postPhoto({ lot_id: 1, photo_id: 2 });
   console.log("MP NOTIFICATION");
 });
 
