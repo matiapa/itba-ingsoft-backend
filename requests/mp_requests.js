2e83c092f92239209ac346429ac310aa49c34a41
MatiMonaco <matimonaco@outlook.com>
2020-07-12 23:21:50 -0300

    mp rquest

--- requests/mp_requests.js
+++ requests/mp_requests.js
@@ -15,20 +15,55 @@ const options = {
   },
 };
 
-const post_request = https.request(options, (res) => {
+const get_preference_id = https.request(options, (res) => {
   if (res.statusCode == 201) {
     let data = "";
     res.on("data", (chunk) => {
       data += chunk.toString("utf8");
     });
-    res.on("");
+    res.on("end", () => {
+      console.log(JSON.parse(data));
+      //TODO mandar evento de cerrado
+    });
   } else {
     console.log("Mercado Pago API Error:");
   }
 });
+get_preference_id.on("error", (error) => {
+  console.error(error);
+});
 
 module.exports = {
-  createPreference(info) {
-    const postData = info;
+  createPreference(auction, highestBid, payer_info) {
+    const data = JSON.stringify({
+      items: [
+        {
+          title: auction.name,
+          description: auction.description,
+          quantity: auction.quantity,
+          currency_id: "ARS",
+          unit_price: highestBid,
+        },
+      ],
+      payer: {
+        name: payer_info.name,
+        surname: payer_info.last_name,
+      },
+      payment_methods: {
+        excluded_payment_methods: [
+          {
+            id: "ticket",
+          },
+          {
+            id: "atm",
+          },
+          {
+            id: "prepaid_card",
+          },
+        ],
+      },
+    });
+    get_preference_id.write(data);
+    get_preference_id.end();
   },
 };
